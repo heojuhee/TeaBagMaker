fee02a1477ba6203f7f1578c666c6cb0357edafa
Juhee Heo <s2017i20@e-mirim.hs.kr>
2019-05-24 11:26:45 +0900

    #1 폼 디자인 내용 추가

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.menu = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -54,16 +55,27 @@
             this.menu.Size = new System.Drawing.Size(248, 20);
             this.menu.TabIndex = 1;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "2019.5.24 허주희 3218 ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(270, 499);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menu);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +83,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox menu;
+        private System.Windows.Forms.Label label1;
     }
 }
 
